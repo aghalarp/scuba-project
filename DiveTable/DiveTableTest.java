d028735bf4ad972236e2291ed72ce9c6c039c696
UHrocks7 <chrisc7@hawaii.edu>
2014-12-05 16:32:02 -1000

    Updates to Test Code, but still working on it
    Was able to add a few more tests. With the 3rd test, I got a
    NullPointerException when I added a second dive after the first dive.
    The first dive had a depth of 100, time of 30, then SIT of 20. The
    second dive had a depth of 40, time of 75, and SIT of 75. Will work on
    testing it more.

--- DiveTable/DiveTableTest.java
+++ DiveTable/DiveTableTest.java
@@ -1,10 +1,15 @@
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.experimental.results.PrintableResult;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
+
 
 /**
  * DiveTableTest
@@ -16,6 +21,8 @@ public class DiveTableTest {
 
 	//private final String makePlain = "\033[0;0m";
 	//private final String makeBold = "\033[0;1m";
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
 	
 	@Test
 	public void checkDiveTableIfNotNull() {
@@ -24,38 +31,21 @@ public class DiveTableTest {
 		assertNotNull("'endOfDiveTable' is null.", diveTable.endOfDiveTable);
 		if (diveTable.endOfDiveTable != null) {
 			System.out.println("assertNotNull: 'endOfDiveTable' is OK.");
-			//System.out.println(diveTable.endOfDiveTable);
-			/*Map<String, String> revEntry = new LinkedHashMap<String, String>(diveTable.endOfDiveTable.size());
-			for (Map.Entry entry : diveTable.endOfDiveTable.entrySet()) {
-				revEntry.put(entry.getValue(), entry.getKey());
-			}*/
 		}
 		assertNotNull("'repetitiveDiveTimeTable' is null.", diveTable.repetitiveDiveTimeTable);
 		if (diveTable.repetitiveDiveTimeTable != null) {
 			System.out.println("assertNotNull: 'repetitiveDiveTimeTable' is OK.");
-			//System.out.println(diveTable.repetitiveDiveTimeTable);
 		}
 		assertNotNull("'surfaceIntervalTimeTable' is null.", diveTable.surfaceIntervalTimeTable);
 		if (diveTable.surfaceIntervalTimeTable != null) {
 			System.out.println("assertNotNull: 'surfaceIntervalTimeTable' is OK.\n");
-			//System.out.println(diveTable.surfaceIntervalTimeTable);
 		}
-		/*assertNotNull("'diveResultsMap' is null.", diveTable.diveResultsMap);
-		if (diveTable.diveResultsMap != null) {
-			System.out.println("assertNotNull: 'diveResultsMap' is OK.\n");
-			//System.out.println(diveTable.diveResultsMap);
-		}*/
 	}
 	
-	/*@Test
-	public void testDiveTableFormat () {
-		DiveTable diveTable = new DiveTable();
-		//fail("Not yet implemented");
-	}*/
-	
 	@Test
-	public void checkDiveInputsAndCalculations() { 
-		System.out.println("\"Test 2: Checking results of calculations for dive tables\"");
+	//Note that most of this test was copied/transfered from the DiveTable class
+	public void checkDiveInputs() { 
+		System.out.println("\"Test 2: Checking if information is added correctly\"");
         //Testing Dive Plan 1 (Legal)
 		System.out.println("Test Dive Plan 1...");
         List<Integer> testDives = new ArrayList<Integer>();
@@ -92,8 +82,9 @@ public class DiveTableTest {
 
         DiveTable dt = new DiveTable();
         dt.calculateDiveTableResults(testDives);
+        //dt.calculateDiveTableResults();
         System.out.println(dt);
-       
+        
         //Testing Dive Plan 2 (Illegal)
         System.out.println("\n\nTest Dive Plan 2...");
         List<Integer> testDives2 = new ArrayList<Integer>();
@@ -131,6 +122,44 @@ public class DiveTableTest {
         DiveTable dt2 = new DiveTable();
         dt2.calculateDiveTableResults(testDives2);
         System.out.println(dt2);
+        
+	}
+	
+	@Test
+	public void testDiveCalculations() {
+		System.out.println("\n\"Test 3: Checking how program responds to added numbers\"");
+		List<Integer> dive = new ArrayList<Integer>();
+		System.out.println("Dive #1");
+		dive.add(100);
+		assertTrue("Depth was not added correctly",dive.get(0) == 100);
+		dive.add(30);
+		assertTrue("Time was not added correctly",dive.get(1) == 30);
+		dive.add(20);
+		assertTrue("Surface Interval Time was not added correctly",dive.get(2) == 20);
+		
+		System.out.println("Dive #2");
+		dive.add(45);
+		assertTrue("Depth was not added correctly",dive.get(3) == 45);
+		dive.add(75);
+		assertTrue("Time was not added correctly",dive.get(4) == 75);
+		dive.add(75);
+		assertTrue("Surface Interval Time was not added correctly",dive.get(5) == 75);
+		/*
+		System.out.println("Dive #3");
+		dive.add(40);
+		assertTrue("Depth was not added correctly",dive.get(6) == 40);
+		dive.add(30);
+		assertTrue("Time was not added correctly",dive.get(7) == 30);
+		dive.add(88);
+		assertTrue("Surface Interval Time was not added correctly",dive.get(8) == 88);*/
+		
+		DiveTable diveTable = new DiveTable();
+		diveTable.calculateDiveTableResults(dive);
+		assertNotNull(diveTable);
+		PrintableResult.testResult(DiveTable.class);
+		System.out.println(diveTable);
+		
+		
 	}
 	
 }
